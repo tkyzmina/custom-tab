c8a746a2b2407d6bf144de93ce05c6586fdef3ea
tkyzmina <tkyzmina@gmail.com>
2021-04-15 23:28:47 +0300

    добавляет логику

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const accordion = document.querySelectorAll(".accordion");
 
-console.log(accordion);
-
 accordion.forEach(function (item) {
   const btn = item.children[0];
   const content = item.children[1];
   const parent = item.closest(".accordion__content");
-  const maxHeight = content.style.maxHeight;
-  console.log(content.scrollHeight);
+
   btn.addEventListener("click", (evt) => {
     evt.preventDefault();
-    console.log("click");
-    // console.log(maxHeight);
-    content.style.maxHeight = content.scrollHeight + "px";
-    btn.classList.add("active");
+
+    const maxHeight = content.style.maxHeight;
+
+    if (maxHeight) {
+      content.style.maxHeight = null;
+      btn.classList.remove("active");
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      btn.classList.add("active");
+    }
   });
 });
-
-// console.log(accordionControl);
